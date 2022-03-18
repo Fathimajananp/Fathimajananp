e5d7080ccc5d4d9e0fc715c299461cc6ea19a56c
Fathimajananp <95746148+Fathimajananp@users.noreply.github.com>
2022-03-18 14:57:48 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,9 @@
+class circle{
+	public static void main(String args[]){
+		int r=5;
+		double pi=3.14,area;
+		area=pi*r*r;
+		System.out.print("The radius is:"+r);
+		System.out.println("\nArea of the circle is :"+area);
+}
+}
---
+++ fahrenheit.java
@@ -0,0 +1,9 @@
+class fahrenheit{
+	public static void main(String args[]){
+		float f=39,c;
+		System.out.print("The temperature is :"+f);
+		c=(5*(f-32))/9;
+		System.out.println("\nDegree Celcius is:"+c);
+}
+}
+
